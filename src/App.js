3980d122b85e9a540db05de3418ae9b47a7f739e
frewill008 <kashif.sohail@gmail.com>
2024-12-11 13:47:35 +0300

    update auth0
    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { auth0Config } from './auth0-config';
+import { useAuth0 } from '@auth0/auth0-react';
 import EmployeeList from './components/EmployeeList';
+import LoginButton from './components/LoginButton';
 import './App.css';
 
-const client = new ApolloClient({
-  uri: 'YOUR_HASURA_ENDPOINT',
-  cache: new InMemoryCache(),
-  headers: {
-    'x-hasura-admin-secret': 'YOUR_ADMIN_SECRET'
-  }
+// Create Apollo Link with Auth Header
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_HASURA_ENDPOINT,
 });
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from Auth0
+  const token = localStorage.getItem('auth0_token');
+  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+// Protected Route Component
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthenticated ? children : <LoginButton />;
+};
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
-          <h1>Employee Management</h1>
-        </header>
-        <main>
-          <EmployeeList />
-        </main>
-      </div>
-    </ApolloProvider>
+    <Auth0Provider
+      domain={auth0Config.domain}
+      clientId={auth0Config.clientId}
+      redirectUri={auth0Config.redirectUri}
+      audience={auth0Config.audience}
+    >
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <h1>Employee Management</h1>
+          </header>
+          <main>
+            <ProtectedRoute>
+              <EmployeeList />
+            </ProtectedRoute>
+          </main>
+        </div>
+      </ApolloProvider>
+    </Auth0Provider>
   );
 }
 
